4e9de9bd9ebdbc412877a1f03c68be88baf70bf8
Tim <dev@tim.ph>
2016-05-26 12:57:12 -0400

    format config a little.

--- inc/boldgrid-theme-framework-config/config.php
+++ inc/boldgrid-theme-framework-config/config.php
@@ -1,37 +1,34 @@
 <?php
 function boldgrid_theme_framework_config( $boldgrid_framework_configs ) {
-	/**
-	 * General Configs
-	 */
+
 	// Text domain.
 	$boldgrid_framework_configs['theme_name'] = 'boldgrid-diced';
+
 	// Enable Sticky Footer.
 	$boldgrid_framework_configs['scripts']['boldgrid-sticky-footer'] = true;
+
 	// Enable typography controls.
 	$boldgrid_framework_configs['customizer-options']['typography']['enabled'] = true;
+
 	// Enable attribution links.
 	$boldgrid_framework_configs['temp']['attribution_links'] = true;
+
 	// Enable template wrapper.
 	$boldgrid_framework_configs['boldgrid-parent-theme'] = true;
+
 	// Specify the parent theme's name.
 	$boldgrid_framework_configs['parent-theme-name'] = 'prime';
+
 	// Select the footer template to use.
 	$boldgrid_framework_configs['template']['footer'] = '1';
-	// Select the header template to use.
-	$boldgrid_framework_configs['template']['header'] = 'generic';
-	// Assign menus, widgets, and actions to locations in generic header template.
-	$boldgrid_framework_configs['template']['locations']['header'] = array(
-		'6' => array( '[action]boldgrid_site_identity' ),
-		'7' => array( '[menu]tertiary' ),
-		'8' => array( '[widget]boldgrid-widget-1', '[menu]secondary', '[action]boldgrid_primary_navigation' ),
-		'9' => array( '[menu]social' ),
-		'10' => array( '[widget]boldgrid-widget-2' ),
-	);
 
-	/**
-	 * Customizer Configs
-	 */
+	// Select the header template to use.
+	$boldgrid_framework_configs['template']['header'] = '';
+
+	// Enable BoldGrid Color Palette System.
 	$boldgrid_framework_configs['customizer-options']['colors']['enabled'] = true;
+
+	// Set default color palettes for theme.
 	$boldgrid_framework_configs['customizer-options']['colors']['defaults'] = array(
 		array(
 			'default' => true,
