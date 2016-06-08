40d8b4d51a46852a1f78fe7168daa7af9d5dd8ad
Rafael Ramos <rramo012@gmail.com>
2016-06-08 16:18:31 -0400

    Removing hardcoded ids

--- inc/boldgrid-theme-framework-config/config.php
+++ inc/boldgrid-theme-framework-config/config.php
@@ -106,21 +106,12 @@ function boldgrid_theme_framework_config( $boldgrid_framework_configs ) {
 		),
 	);
 
-	// Get Subcategory ID from the Database.
-	$boldgrid_install_options = get_option( 'boldgrid_install_options', array() );
-	$subcategory_id = null;
-	if ( ! empty( $boldgrid_install_options['subcategory_id'] ) ) {
-		$subcategory_id = $boldgrid_install_options['subcategory_id'];
-	}
-
 	// Override Options per Subcategory.
-	switch ( $subcategory_id ) {
-		// Fashion.
-		case 14:
+	switch ( $boldgrid_framework_configs['inspiration']['subcategory_key'] ) {
+		case 'Fashion':
 			$boldgrid_framework_configs['customizer-options']['colors']['defaults'][1]['default'] = true;
 			break;
-		 // General.
-		case 32:
+		case 'General':
 			$boldgrid_framework_configs['customizer-options']['colors']['defaults'][3]['default'] = true;
 			break;
 
